2041a1af155523e41af05c81243ac62246265333
ChristopherLavin-STU <ChristopherLavin-STU@S-7Q1H504-SL18>
2025-01-08 13:01:17 +0000

    Add project files.

---
+++ S00251319JanExam2025/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace S00251319JanExam2025
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
